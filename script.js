d1e54d1ba9ebdf5b000761dc8941452076e29cdd
jinore <125263577+jinore@users.noreply.github.com>
2024-08-27 21:22:06 +0300

    n

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var texts = ["Ohjelmoija", "Editori", "Animaattori"];
-    var index = 0;
-    var changingText = document.getElementById("changing-text");
-  
-    // Aseta aluksi teksti "Ohjelmoija"
-    changingText.textContent = texts[index];
-    index++; // Siirry seuraavaan tekstiin
-  
-    setInterval(function() {
-      changingText.textContent = texts[index];
-      index = (index + 1) % texts.length;
-    }, 2000); // Tekstin vaihtumisen väli 2 sekuntia
+document.getElementById('projectsbtn').addEventListener('click', function() {
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('educationbtn').addEventListener('click', function() {
+  document.getElementById('education').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contactbtn').addEventListener('click', function() {
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
 });
